f7faf808e51a05155b41085383ff211371415bc1
ConnerH50 <crh50@students.uwf.edu>
2022-02-07 21:26:23 -0600

    added host stuff, not sure what its for yet

--- httpServer.cpp
+++ httpServer.cpp
@@ -24,6 +24,9 @@
 #include <sys/types.h>
 #include <sys/sendfile.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 /*
  * Namespaces
@@ -50,7 +53,10 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-	int newSocket, serverFD, pid;
+	int newSocket, serverFD, pid, hostName;
+	char hostBuffer[256];
+	char *IPBuffer;
+	struct hostent *hostEntry;
 	struct sockaddr_in sockAddress;
 	int addressLength = sizeof(sockAddress);
 
@@ -58,13 +64,35 @@ int main(int argc, char **argv){
 	sockAddress.sin_family = AF_INET;
 	sockAddress.sin_addr.s_addr = INADDR_ANY; //set ip of address
 	sockAddress.sin_port = htons(PORT); //set port of address
+	memset(sockAddress.sin_zero, '\0', sizeof sockAddress.sin_zero); // set last struct variable
 
+	//get host things
+	hostName = gethostname(hostBuffer, sizeof(hostBuffer)); //get host name
+	hostEntry = gethostbyname(hostBuffer); //get host information. Needed to get IP address
+	IPBuffer = inet_ntoa(*((struct in_addr*)hostEntry->h_addr_list[0])); //convert network into string
+
+
+	// create serverFD w. error checking
 	if((serverFD = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		cout << "Error cannot create socket" << endl;
 		return 0;
+	}
+
+	//Bind the serverFD
+	if(bind(serverFD, (struct sockaddr *)&sockAddress, sizeof(sockAddress)) < 0){
+		cout << "Error in bind" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	//listen on socket
+	if(listen(serverFD, 10) < 0){
+		cout << "error in listen" << endl;
+		exit(EXIT_FAILURE);
 	}	
 
 	cout << "Port is: " << PORT << endl;
+	cout << "Hostname is: " << hostBuffer << endl;
+	cout << "Host IP is: " << IPBuffer << endl;
 
 	return 0;
 }
