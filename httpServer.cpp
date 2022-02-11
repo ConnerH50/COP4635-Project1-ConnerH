f39f9d92f0d49cc8a977281562e46cf709d8b143
Conner Hendricks <crh50@students.uwf.edu>
2022-02-11 15:48:29 -0800

    added some stuff

--- httpServer.cpp
+++ httpServer.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
 #include <condition_variable> //May not need
 
 /*
@@ -35,16 +36,63 @@
 using namespace std;
 
 /*
- * DEFINES
+ * DEFINES and other stuff
  */
 
 #define PORT 60069
+char http_header[25] = "HTTP/1.1 200 Ok\r\n";
+
+
 
 /*
- * Server Class
+ * Methods
  */
+ 
+char *parseRequestType(char firstRequest[], const char sym[]){
+	char *copy = (char *)malloc(strlen(firstRequest) + 1);
+    	strcpy(copy, firstRequest);
+        
+    	char *requestType;
+    	char *token = strtok(copy, sym);
+	int current = 0;
 
-
+    	while( token != NULL ) {
+    	
+      		if(current == 0){
+        		requestType = token;
+          		if(requestType == NULL){
+              			strcpy(requestType, "");
+          		}
+          		free(copy);
+          		return requestType;
+      		}
+      		current = current + 1;
+   	}
+   	free(copy);
+   	free(token);
+   	return requestType; 
+ 
+}
+ 
+char *parseSecond(char request[], const char sym[]){
+	//char *copyOfRequest = (char *)malloc(strlen(request) + 1);
+	char *parsedRequest;
+	char *token = strtok(request, sym);
+	int currentPlace = 0;
+	
+	while(token != NULL){
+		token = strtok(NULL, " ");
+		if(currentPlace == 0){
+			parsedRequest = token;
+			return parsedRequest;
+		}
+		currentPlace++;
+	}
+	
+	
+	//free(copyOfRequest);
+	return parsedRequest;
+}
 
 
 /*
@@ -55,10 +103,16 @@ int main(int argc, char **argv){
 
 	int newSocket, serverFD, pid, hostName, valueRead;
 	char hostBuffer[1024];
+	char buffer[1024];
 	char *IPBuffer;
 	struct hostent *hostEntry;
 	struct sockaddr_in sockAddress;
 	int addressLength = sizeof(sockAddress);
+	
+	//get host things
+	hostName = gethostname(hostBuffer, sizeof(hostBuffer)); //get host name
+	hostEntry = gethostbyname(hostBuffer); //get host information. Needed to get IP address
+	IPBuffer = inet_ntoa(*((struct in_addr*)hostEntry->h_addr_list[0])); //convert network into string
 
 	// set address variables
 	sockAddress.sin_family = AF_INET;
@@ -66,28 +120,23 @@ int main(int argc, char **argv){
 	sockAddress.sin_port = htons(PORT); //set port of address
 	memset(sockAddress.sin_zero, '\0', sizeof sockAddress.sin_zero); // set last struct variable
 
-	//get host things
-	hostName = gethostname(hostBuffer, sizeof(hostBuffer)); //get host name
-	hostEntry = gethostbyname(hostBuffer); //get host information. Needed to get IP address
-	IPBuffer = inet_ntoa(*((struct in_addr*)hostEntry->h_addr_list[0])); //convert network into string
-
 
 	// create serverFD w. error checking
 	if((serverFD = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-		cout << "Error cannot create socket" << endl;
-		return 0;
+		perror("Error cannot create socket");
+		return -1;
 	}
 
 	//Bind the serverFD
 	if(bind(serverFD, (struct sockaddr *)&sockAddress, sizeof(sockAddress)) < 0){
-		cout << "Error in bind" << endl;
-		exit(EXIT_FAILURE);
+		perror("Error in bind");
+		return -1;
 	}
 
 	//listen on socket
 	if(listen(serverFD, 10) < 0){
-		cout << "error in listen" << endl;
-		exit(EXIT_FAILURE);
+		perror("error in listen");
+		return -1;
 	}
 
 	cout << "Port is: " << PORT << endl;
@@ -111,14 +160,42 @@ int main(int argc, char **argv){
 		
 		if(pid == 0){ //child 
 			//send to server
-			valueRead = read(newSocket , hostBuffer, 1024); 
-	    		printf("%s", hostBuffer ); 
+			///*
+			valueRead = read(newSocket , buffer, 1024); 
+	    		cout << buffer << endl;
+	    		
+	    		
+	    		
+	    		send(newSocket , "Welcome to server!" , strlen("Welcome to Server!") , 0); 
+	    		
+	    		close(newSocket);//*/
+	    		
+	    		/*valueRead = read(newSocket , buffer, 1024); // info from client
+			
+	    		//cout << buffer << endl; // prints what client wants?
+	    		char *requestType = parseRequestType(buffer, " ");
+	    		//cout << "Client request type is: " << requestType << endl;
+	    		
+	    		
+	    		char *parsedRequest = parseSecond(buffer, " ");
+	    		//cout << "Parsed Request is: " << parsedRequest << endl;
+	    		
+	    		//copy parsedRequest to get any file types
+	    		char *copyOfParsedRequest;
+	    		strcpy(copyOfParsedRequest, parsedRequest);
+	    		
+	    		if(strcmp(requestType, "GET") == 0){
+	    			//cout << "requestType is GET" << endl;
+	    			
+	    		}
+	    		
 	    		send(newSocket , "Welcome to server!" , strlen("Welcome to Server!") , 0 ); 
 	    		printf("Hello message sent");
 	    		
-	    		close(newSocket);
+	    		close(newSocket);//*/
+	    		
 	    	}else{
-	    		cout << "parent create child with pid: " << pid << endl;
+	    		//cout << "parent create child with pid: " << pid << endl;
 	    		close(newSocket);
 	    	}
 
